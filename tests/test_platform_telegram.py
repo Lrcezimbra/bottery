0da231e83fc112225cac8137ba484803341886cf
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2018-01-24 15:49:56 +0000

    Fix KeyError when Telegram message has no text

--- tests/test_platform_telegram.py
+++ tests/test_platform_telegram.py
@@ -33,6 +33,33 @@ def message():
     )
 
 
+@pytest.fixture()
+def data_message():
+    return {
+        'message': {
+            'chat': {
+                'first_name': 'Lucas',
+                'id': 12345678,
+                'last_name': 'Rangel Cezimbra',
+                'type': 'private',
+                'username': 'Lrcezimbra'
+            },
+            'date': 1516787847,
+            'from': {
+                'first_name': 'Lucas',
+                'id': 12345678,
+                'is_bot': False,
+                'language_code': 'en-US',
+                'last_name': 'Rangel Cezimbra',
+                'username': 'Lrcezimbra'
+            },
+            'message_id': 2,
+            'text': 'Hi bot, how are you?'
+        },
+        'update_id': 987456321
+    }
+
+
 @pytest.mark.parametrize('chat_type,id_expected', [
     ('group', 456),
     ('private', 123),
@@ -43,3 +70,31 @@ def test_platform_telegram_engine_get_chat_id(chat_type,
     setattr(message.chat, 'type', chat_type)
     setattr(message.user, 'id', id_expected)
     assert engine.get_chat_id(engine, message) == id_expected
+
+
+def test_build_message(engine, data_message, user, chat):
+    message_data = data_message['message']
+
+    message = engine.build_message(engine, data_message)
+
+    assert message.id == message_data['message_id']
+    assert message.text == message_data['text']
+    assert message.timestamp == message_data['date']
+    assert message.raw == data_message
+
+
+def test_build_message_without_text(data_message, engine):
+    '''
+    Telegram can send a message without text.
+    For example, when a bot is added to a group.
+    '''
+    message_data_without_text = data_message['message']
+    del message_data_without_text['text']
+
+    message = engine.build_message(engine, data_message)
+
+    assert message.id == message_data_without_text['message_id']
+    assert message.text is not None
+    assert message.text == ''
+    assert message.timestamp == message_data_without_text['date']
+    assert message.raw == data_message
